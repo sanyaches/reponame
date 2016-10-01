22bda13ea0aa99ef53f4239145d25c2792160f06
sanyaches <sanya_ches@mail.ru>
2016-10-01 17:24:52 +0500

    доделал задачку

--- laba1z1/laba1z1/Program.cs
+++ laba1z1/laba1z1/Program.cs
@@ -10,37 +10,13 @@ namespace laba1z1
     {
         static void Main(string[] args)
         {
-            int n,j,m;
-           string s;
-            Console.Write("Enter n = ");
+            int n,m;
+            Console.Write("Введите n = ");
             n = int.Parse(Console.ReadLine());
-           Console.Write("Our m = ");
-            j = n;
-            char [] array= j.ToString().Reverse();
-            Console.WriteLine(s);
-            //m = int.Parse(s);
-            //Console.Write(m);
-            //if(n==m)
-           //     Console.WriteLine("Число палиндром");
-            //else Console.WriteLine("Число не палиндром");
-            /*for(int i=0;i<j/2;i++)
-            {
-                if (array[i] == array[j - i])
-                {
-                    Console.WriteLine("Число палиндром");
-                    continue;
-                }
-                else
-                {
-                    Console.WriteLine("Число не палиндром");
-                    break;
-                }
-                
-            }*/
-            /* Console.Write(n % 10);
-             while ((n /= 10) != 0)
-                 Console.Write(n % 10);*/
-
+            m = int.Parse(new string(n.ToString().Reverse().ToArray()));
+            Console.WriteLine("Наше число n в обратном порядке = {0}", m);
+            if (n==m) Console.WriteLine("число n палиндром");
+            else Console.WriteLine("Число n не палиндром"); 
         }
     }
 }
